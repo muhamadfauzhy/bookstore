5185ac0c836f02bb1f6d403c3e4e525ef72708d4
muhamadfauzhy <fauzhyputra0714@gmail.com>
2026-04-01 16:50:37 +0700

    sessiom

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = require('./routes/index')
 const app = express()
+const session = require ('express-session')
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(session ({ 
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false,
+    sameSite: true 
+  }
+}))
+
 app.use("/", router)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
-
+const Controller = require('../controllers/controller')
 const bookRoutes = require('./books')
 const purchaseRoutes = require('./purchases')
+
+router.get('/register', Controller.getRegister)
+router.post('/register', Controller.postRegister)
+
+router.get('/login', Controller.getLogin)
+router.post('/login', Controller.postLogin)
+
+router.use (function (req, res, next) {
+  if (!req.session.userId) {
+    const error = 'Please login first!'
+    res.redirect (`/login?error=${error}`)
+  } else {
+    next ()
+  }
+})
 
 // route utama
 router.get('/', (req, res) => {
